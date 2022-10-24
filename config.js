06758fba3521842cd2dad564c828001b66111f44
Tatangkalan <116570798+Tatangkalan@users.noreply.github.com>
2022-10-24 21:06:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6283162281324']
+global.ownernomer = "6283162281324"
+global.premium = ['6283162281324']
 global.packname = 'Sticker By'
 global.author = 'Naze Dev'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
